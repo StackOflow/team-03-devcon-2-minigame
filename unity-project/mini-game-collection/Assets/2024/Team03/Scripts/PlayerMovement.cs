4c32cc2e37bc7025d4233bf6329e7a1172737b0b
Jonah Cappa <jonahcappaofficial@gmail.com>
2024-11-07 17:43:17 -0500

    Add player movement script

---
+++ unity-project/mini-game-collection/Assets/2024/Team03/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+//using System.Numerics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public Transform transform;
+
+    public float movementSpeed = 0.5f;
+    //public Vector3 playerStartPos;
+    //public Vector3 playerStartRot;
+
+    private void Start()
+    {
+        //playerStartPos = new Vector3(22.8260002f, 1.53900003f, -8.89000034f);
+        //playerStartRot = new Vector3(0, 0, 0);
+    }
+
+
+
+    void Update()
+    {
+        MoveRight();
+    }
+
+
+
+    //Movements
+    public void MoveRight()
+    {
+        transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+    }
+
+    public void MoveBackwards()
+    {
+        transform.position = transform.position + new Vector3(0, 0, -movementSpeed * Time.deltaTime);
+
+
+    }
+}
