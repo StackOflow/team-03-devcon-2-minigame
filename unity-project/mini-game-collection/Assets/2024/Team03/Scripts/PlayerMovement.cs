e54f5cf4ef8fc065f24681c224d271e5c60126f9
Park <cjparkerdj@gmail.com>
2024-11-07 18:08:33 -0500

    Added namespace to script

--- unity-project/mini-game-collection/Assets/2024/Team03/Scripts/PlayerMovement.cs
+++ unity-project/mini-game-collection/Assets/2024/Team03/Scripts/PlayerMovement.cs
@@ -4,40 +4,43 @@ using System.Diagnostics;
 //using System.Numerics;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace MiniGameCollection.Games2024.Team03
 {
-    
-    public Transform transform;
-
-    public float movementSpeed = 0.5f;
-    //public Vector3 playerStartPos;
-    //public Vector3 playerStartRot;
-
-    private void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        //playerStartPos = new Vector3(22.8260002f, 1.53900003f, -8.89000034f);
-        //playerStartRot = new Vector3(0, 0, 0);
-    }
+
+        public Transform transform;
+
+        public float movementSpeed = 0.5f;
+        //public Vector3 playerStartPos;
+        //public Vector3 playerStartRot;
+
+        private void Start()
+        {
+            //playerStartPos = new Vector3(22.8260002f, 1.53900003f, -8.89000034f);
+            //playerStartRot = new Vector3(0, 0, 0);
+        }
 
 
 
-    void Update()
-    {
-        MoveRight();
-    }
+        void Update()
+        {
+            MoveRight();
+        }
 
 
 
-    //Movements
-    public void MoveRight()
-    {
-        transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
-    }
+        //Movements
+        public void MoveRight()
+        {
+            transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+        }
 
-    public void MoveBackwards()
-    {
-        transform.position = transform.position + new Vector3(0, 0, -movementSpeed * Time.deltaTime);
+        public void MoveBackwards()
+        {
+            transform.position = transform.position + new Vector3(0, 0, -movementSpeed * Time.deltaTime);
 
 
+        }
     }
 }
