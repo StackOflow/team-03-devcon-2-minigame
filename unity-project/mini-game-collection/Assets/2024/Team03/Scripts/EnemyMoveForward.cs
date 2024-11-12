dddad7f36fbffea3760ac909173a314f609506ee
Park <cjparkerdj@gmail.com>
2024-11-12 18:24:23 -0500

    Updated Enemy Movement Script

--- unity-project/mini-game-collection/Assets/2024/Team03/Scripts/EnemyMoveForward.cs
+++ unity-project/mini-game-collection/Assets/2024/Team03/Scripts/EnemyMoveForward.cs
@@ -6,23 +6,49 @@ namespace MiniGameCollection.Games2024.Team03
 {
     public class EnemyMoveForward : MonoBehaviour
     {
-        public Animator enemyAnim;
-
-
         public float speed = 5f; // Speed at which the GameObject moves forward
         private Vector3 forward = new Vector3(0, 0, 1);
+        private Vector3 pause = new Vector3(0, 0, 0);
+
+        private Transform player;           // Reference to the player's position 
+        public LayerMask playerLayer;
+
+        private void Start()
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+            // If player object is found, store its transform
+            if (playerObject != null)
+            {
+                player = playerObject.transform;
+            }
+        }
 
         void Update()
         {
-            // Move the GameObject forward along its local z-axis
-            transform.Translate(forward * speed * Time.deltaTime);
+            if (player == null)
+            {
+                return;
+            }
+
+            // Cast a ray from the enemy's position to the player
+            Vector3 directionToPlayer = player.position - transform.position;
+            Ray ray = new Ray(transform.position, directionToPlayer);
+            RaycastHit hit;
+
+            // Perform the raycast to see if the player is within the detection range
+            if (Physics.Raycast(ray, out hit, 0.7f, playerLayer))
+            {
+                transform.Translate(pause);
+            }
+            else
+            {
+                // Move the GameObject forward along its local z-axis
+                transform.Translate(forward * speed * Time.deltaTime);
+            }
         }
 
 
-        public void Animations()
-        {
-            enemyAnim.SetBool("isRunning", true);
-        }
 
 
     }
